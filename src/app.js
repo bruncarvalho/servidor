1797014cdbaef7f77526f81b9a3106f2b5445d9f
Bruna Carvalho <brunacarvalhocardoso@gmail.com>
2022-09-12 18:55:23 -0300

    rota post

--- src/app.js
+++ src/app.js
@@ -9,12 +9,16 @@ app.use(router)
 
 router.get('/api', (request, response)=> {
   response.json({
-    messege: 'Hello World'
+    messege: 'Gatos'
   })
 })
 
-// router.post('/api', ()=> {
-//   request 
-// })
-
+router.post('/api', (request, response)=>{
+  const { nameCat, ageCat } = request.body
+  const animal = {
+    name: nameCat,
+    age: ageCat
+  } 
+  return response.json(animal)
+})
 app.listen('3333', ()=> console.log('servidor funcionando: porta:3333'))
